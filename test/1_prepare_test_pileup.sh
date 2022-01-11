077fbe3e4cdcce94347d8b7bc1b5d7ebda635421
jeff <jeffersonparil@gmail.com>
2022-01-11 21:48:29 +1100

    trying to download ENA poolseq data

--- test/1_prepare_test_pileup.sh
+++ test/1_prepare_test_pileup.sh
@@ -8,3 +8,20 @@ grep 'scaffold_31709' ${DIR_2}/Lolium2019.mpileup >> ${DIR_1}/test.mpileup
 
 ### Download Pool-seq data from Drosophila and Human cancer cells; and also 
 
+sudo apt install -y curlftpfs
+
+### Drosophila
+for d in Drosophila-ERR173 Drosophila-ERR115 Human-SRR350
+do
+    # d=Drosophila-ERR173
+    mkdir ${d}
+    curlftpfs -r ftp.sra.ebi.ac.uk/vol1/fastq/ERR173 Drosophila-ERR173/
+    time find Drosophila-ERR173/ -path '*.fastq.gz' > fname_sequences-${d}.temp
+    fusermount -u ${d}
+    mv fname_sequences-${d}.temp ${d}
+    cd ${d}
+    sed -i "s/${d%-*}-/ftp.sra.ebi.ac.uk\/vol1\/fastq\//g" fname_sequences-${d}.temp
+    # f=Drosophila-ERR173/ERR173999/ERR173999.fastq.gz
+    time parallel wget {} ::: $(cat fname_sequences-${d}.temp)
+done
+
