644cd264e93645b84e1ef9c3d656827e65981b46
jeffersonfparil <jeffersonparil@gmail.com>
2022-01-18 15:30:44 +1100

    fixing missing data simulation function by checking scaffold/chromosome name together with the position

--- test/test_CI.jl
+++ test/test_CI.jl
@@ -6,16 +6,16 @@ using UnicodePlots
 using Distributed
 n_int_thread_count = 2 ### guthub actions virtual machine allocated has only 2 cores
 Distributed.addprocs(n_int_thread_count)
-# Pkg.add(url="https://github.com/jeffersonfparil/PoPoolImpute.jl.git")
-# @everywhere using PoPoolImpute
+Pkg.add(url="https://github.com/jeffersonfparil/PoPoolImpute.jl.git")
+@everywhere using PoPoolImpute
 
 ### Navigate to testing directory
 cd("test/")
 
 ################################
 ### TEST LOCALLY: comment-out lines 7 and 8 first
-@everywhere include("/home/jeffersonfparil/Documents/PoPoolImpute.jl/src/PoPoolImpute.jl")
-cd("/home/jeffersonfparil/Documents/PoPoolImpute.jl/test")
+# @everywhere include("/home/jeffersonfparil/Documents/PoPoolImpute.jl/src/PoPoolImpute.jl")
+# cd("/home/jeffersonfparil/Documents/PoPoolImpute.jl/test")
 ################################
 
 ### simulate missing loci, output a pileup file with missing data, and return the total number of loci
