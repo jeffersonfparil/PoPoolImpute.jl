35b3cb64030a54ad588ae486b5b3fdaa3793e01e
jeffersonfparil <jeffersonparil@gmail.com>
2022-02-08 18:24:39 +1100

    probably a massive waste of effort as means are better than any of them fancy methods

--- test/PoPoolImpute-ACCURACY-ANALYSIS.r
+++ test/PoPoolImpute-ACCURACY-ANALYSIS.r
@@ -1,6 +1,42 @@
 ### Analyse imputation accuracies
 
-fname_ctDNA_tar_gz = "PoPoolImpute-ACCURACY-ctDNA.csv.tar.gz" 
+### Concatenate output and insert model info
+```{julia}
+using ProgressMeter
+DIR="/data-weedomics-1/ctDNA"
+cd(DIR)
+vec_str_filenames = readdir()
+vec_str_filenames = vec_str_filenames[match.(Regex("ACCURACY"), vec_str_filenames) .!= nothing]
+@showprogress for str_filename in vec_str_filenames
+    # str_filename = vec_str_filenames[1]
+    str_dist, str_model, str_rep = split(str_filename, '-')[(end-2):end]
+    str_rep = split(str_rep, '.')[1]
+    FILE = open(str_filename, "r")
+    file = open(string(str_filename, ".temp"), :"w")
+    header = readline(FILE)
+    header = string("Rep,Model,Distance_PCs,", header, "\n")
+    write(file, header)
+    while !eof(FILE)
+        line = readline(FILE)
+        line = string(join([str_rep,str_model,str_dist, line], ','), "\n")
+        write(file, line)
+    end
+    close(FILE)
+    close(file)
+    mv(string(str_filename, ".temp"), str_filename, force=true)
+end
+```
+```{sh}
+f=$(ls *ACCURACY*.csv | head -n1)
+head -n1 $f > PoPoolImpute-OUTPUT-ctDNA.csv
+for f in $(ls *ACCURACY*.csv)
+do
+    tail -n+2 $f >> PoPoolImpute-OUTPUT-ctDNA.csv
+done
+```
+
+
+fname_ctDNA_tar_gz = "PoPoolImpute-OUTPUT-ctDNA.csv.tar.gz" 
 system(paste0("tar -xzf ", fname_ctDNA_tar_gz))
 fname_ctDNA = paste(rev(tail(rev(unlist(strsplit(fname_ctDNA_tar_gz, "[.]"))),-2)), collapse='.')
 
@@ -10,8 +46,8 @@ str(dat)
 fun_filter_data_and_plot = function(data, vec_bool_filter=rep(TRUE, nrow(dat))){
     subdat = data[vec_bool_filter, ]
 
-    RMSE_counts = sqrt(mean((subdat$True_counts - subdat$Imputed_counts)^2))
-    RMSE_freqs = sqrt(mean((subdat$True_freqs - subdat$Imputed_freqs)^2))
+    RMSE_counts = sqrt(mean((subdat$True_counts - subdat$Imputed_counts)^2, na.rm=TRUE))
+    RMSE_freqs = sqrt(mean((subdat$True_freqs - subdat$Imputed_freqs)^2, na.rm=TRUE))
 
     par(mfrow=c(1,2))
     plot(subdat$True_counts, subdat$Imputed_counts, type="p", pch=19, col=rgb(0.26,0.63,0.79,alpha=0.5), xlab="Expected", ylab="Imputed", main="Counts")
@@ -25,8 +61,25 @@ fun_filter_data_and_plot = function(data, vec_bool_filter=rep(TRUE, nrow(dat))){
     legend("topleft", legend=paste0("RMSE = ", round(RMSE_freqs,4)))
 }
 
-fun_filter_data_and_plot(data=dat)
-fun_filter_data_and_plot(data=dat, vec_bool_filter=c((dat$Depth >=100) & (dat$Depth <=1e6)))
+# fun_filter_data_and_plot(data=dat)
+# fun_filter_data_and_plot(data=dat, vec_bool_filter=c((dat$Depth >=100) & (dat$Depth <=1e6)))
+for (model in unique(dat$Model)){
+    if ((model=="Mean") | (model=="OLS")){
+        vec_bool_filter=c((dat$Model==model) & (dat$Depth >=100) & (dat$Depth <=1e6))
+        png(paste0(model, "-distPCs_false-scatterplots.png"), width=1000, height=700)
+        fun_filter_data_and_plot(data=dat, vec_bool_filter=vec_bool_filter)
+        dev.off()
+    } else {
+        for (dist in c("false", "true")){
+            vec_bool_filter=c((dat$Model==model) & (dat$Distance_PCs==dist) & (dat$Depth >=100) & (dat$Depth <=1e6))
+            png(paste0(model, "-distPCs_", dist, "-scatterplots.png"), width=1000, height=700)
+            fun_filter_data_and_plot(data=dat, vec_bool_filter=vec_bool_filter)
+            dev.off()
+        }
+        
+    }
+}
+
 
 
 
